86028ab4aa926a5d11048b4af592e3ea1fdbbfa1
bernardw42 <bernard.wijaya42@gmail.com>
2024-07-30 22:32:31 +0700

    Finalized again

--- __mocks__/expo-camera.tsx
+++
@@ -1,5 +0,0 @@
-export const useCameraPermissions = jest.fn().mockReturnValue({
-    permission: { granted: true }, // Adjust as per your test needs
-    requestPermission: jest.fn().mockResolvedValue({ status: 'granted' }), // Adjust as per your test needs
-  });
-  
